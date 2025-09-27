c59747a288673998e62fbfc6dbf416fa752e0486
Sumit Anand <anandsumit2000@gmail.com>
2025-09-27 09:41:18 +0530

    infra: add bootstrap note to backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,7 @@
+# Remote state in S3 with DynamoDB locking.
+# Run the bootstrap commands in README before `terraform init`:
+#   aws s3 mb s3://k8s-threat-locator-tfstate
+#   aws dynamodb create-table --table-name k8s-threat-locator-tflock ...
 terraform {
   backend "s3" {
     bucket         = "k8s-threat-locator-tfstate"
