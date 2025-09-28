93ad0b621bcb5c1104a421bc73d6c2d7b351c4c9
Sumit Anand <anandsumit2000@gmail.com>
2025-09-28 15:16:55 +0530

    infra: use mutability variable in ecr repository resource

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -8,7 +8,7 @@ locals {
 
 resource "aws_ecr_repository" "this" {
   name                 = var.repository_name
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
     scan_on_push = true
