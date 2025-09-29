c509ef0507c6c4f5d6459701c317f5dab54cda0e
Sumit Anand <anandsumit2000@gmail.com>
2025-09-29 10:30:43 +0530

    infra: add public and private subnets across two azs

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -43,3 +43,71 @@ resource "aws_subnet" "private" {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   })
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-igw"
+  })
+}
+
+resource "aws_eip" "nat" {
+  count  = length(var.public_subnet_cidrs)
+  domain = "vpc"
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-nat-eip-${count.index}"
+  })
+}
+
+resource "aws_nat_gateway" "this" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-nat-${var.azs[count.index]}"
+  })
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-public-rt"
+  })
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-private-rt-${var.azs[count.index]}"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
