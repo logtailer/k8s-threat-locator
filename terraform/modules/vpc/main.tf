fc684439f8629a5be09fb8710e31acf589ad8084
Sumit Anand <anandsumit2000@gmail.com>
2025-09-30 14:30:08 +0530

    infra: add route tables and subnet associations

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -113,3 +113,16 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = aws_route_table.private[*].id
+
+  tags = merge(local.tags, {
+    Name = "${var.project}-s3-endpoint"
+  })
+}
+
+data "aws_region" "current" {}
