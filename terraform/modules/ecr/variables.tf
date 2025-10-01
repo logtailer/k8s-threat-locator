6a8c51c0456b65977e96b30b8fcc6c8d9b50ee42
Sumit Anand <anandsumit2000@gmail.com>
2025-10-01 11:30:14 +0530

    infra: scaffold ecr module files

---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,12 @@
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
