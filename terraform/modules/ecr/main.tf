5edae0022692d9de4364bdea985650a8968b8517
Sumit Anand <anandsumit2000@gmail.com>
2025-10-02 11:00:51 +0530

    infra: set ecr image tag mutability to mutable initially

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -18,3 +18,25 @@ resource "aws_ecr_repository" "this" {
     Name = var.repository_name
   })
 }
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 14 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 14
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
