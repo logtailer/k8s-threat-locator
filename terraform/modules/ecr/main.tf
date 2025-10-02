2ccd93386934d6508919bdd61f9691a3f0ae4953
Sumit Anand <anandsumit2000@gmail.com>
2025-10-02 14:30:07 +0530

    infra: add ecr lifecycle policy to expire untagged images

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -26,6 +26,19 @@ resource "aws_ecr_lifecycle_policy" "this" {
     rules = [
       {
         rulePriority = 1
+        description  = "Keep only the last 10 tagged images"
+        selection = {
+          tagStatus   = "tagged"
+          tagPrefixList = ["v", "sha"]
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
         description  = "Expire untagged images after 14 days"
         selection = {
           tagStatus   = "untagged"
