591e1b3693f2299c0ee0e180348a8560a88a4cdf
Sumit Anand <anandsumit2000@gmail.com>
2025-10-06 09:00:31 +0530

    infra: scaffold eks module files

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,52 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.29"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC to deploy the cluster into"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "IDs of the private subnets for node group placement"
+  type        = list(string)
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for the managed node group"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "desired_nodes" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "min_nodes" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_nodes" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
