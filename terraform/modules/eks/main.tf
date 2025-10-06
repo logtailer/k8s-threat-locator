28830ed0496a022c38031c5e2a3608a998218eba
Sumit Anand <anandsumit2000@gmail.com>
2025-10-06 13:00:44 +0530

    infra: add eks cluster resource with private endpoint enabled

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -29,3 +29,23 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  version  = var.cluster_version
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = false
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+
+  tags = merge(local.tags, {
+    Name = var.cluster_name
+  })
+
+  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
+}
