4c764f0aca7ff39bf798aa10af471d3c7f5e8389
Sumit Anand <anandsumit2000@gmail.com>
2025-10-08 09:15:36 +0530

    infra: enable oidc provider on eks for irsa support

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -79,6 +79,20 @@ resource "aws_iam_role_policy_attachment" "node_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  tags = merge(local.tags, {
+    Name = "${var.cluster_name}-oidc"
+  })
+}
+
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-nodes"
