96720338a3d0813cd250b9516369b224760992c3
Sumit Anand <anandsumit2000@gmail.com>
2025-10-12 15:41:08 +0530

    infra: expose s3 policy arn from irsa module outputs

--- terraform/modules/irsa/outputs.tf
+++ terraform/modules/irsa/outputs.tf
@@ -7,3 +7,8 @@ output "role_name" {
   description = "Name of the IAM role"
   value       = aws_iam_role.app.name
 }
+
+output "s3_policy_arn" {
+  description = "ARN of the managed S3 read policy attached to the IRSA role"
+  value       = aws_iam_policy.app_s3.arn
+}
