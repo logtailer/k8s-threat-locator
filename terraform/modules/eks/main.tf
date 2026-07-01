add5324572fb8f27c6f086bd29df582b7a979a72
Sumit Anand <anandsumit2000@gmail.com>
2026-07-01 09:20:00 +0530

    infra: switch eks auth mode to api-only to remove configmap attack surface

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -42,7 +42,7 @@ resource "aws_eks_cluster" "this" {
   }
 
   access_config {
-    authentication_mode                         = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
